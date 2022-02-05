ce401c80cd4434fef943b4b9883ee74f70f45b34
vpuzyrevich <v.puzyrevich@gmail.com>
2022-02-05 22:27:48 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const tens = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const teens=['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    if(number === 0) {
+        return 'zero';
+    } else if (number < 10) {
+        return ones[number];
+    } else if(number >= 10 && number < 20) {
+        return `${teens[number % 10]}`;
+    } else if (number >= 20 && number < 100 && number % 10 === 0) {
+        return `${tens[Math.floor(number/10)]}`;
+    } else if (number >= 20 && number < 100) {
+        return `${tens[Math.floor(number/10)]} ${ones[number % 10]}`;
+    } else if(number % 100 === 0) {
+        return `${ones[Math.floor(number/100)]} hundred`;
+    } else if((number % 100) < 10) {
+        return `${ones[Math.floor(number/100)]} hundred ${ones[number % 100]}`;
+    } else if((number % 100) < 20 && (number % 100) > 10) {
+        return `${ones[Math.floor(number/100)]} hundred ${teens[number % 10]}`;
+    } else if(number % 10 === 0) {
+        return `${ones[Math.floor(number/100)]} hundred ${tens[Math.floor(number % 100 / 10)]}`;
+    } else {
+        return `${ones[Math.floor(number/100)]} hundred ${tens[Math.floor(number % 100 / 10)]} ${ones[number % 10]}`;
+    }
+};
